3971871722a2dcae59a73f3bb02b879f9ce5e4bb
Fabian Rojas <fabian.rojas02@alumnos.ucn.cl>
2021-12-10 04:02:05 -0300

    feat: add prime factor finder

--- fibonacci.c
+++ fibonacci.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>
-#include<stdbool.h>
+#include <stdbool.h>
+#include <math.h>
+
+int factorsArray[10];
+int potArray[10];
+int currentId = 0;
 
 void reset () {
     printf("\033[0m");
@@ -45,6 +50,80 @@ bool isPrime(int n) {
     return true;
 }
 
+void primesFactors(int number) {
+    if (number == 0) return;
+
+    for (int i = 1; i <= number; ++i) {
+        if (number % i == 0) {
+            if (isPrime(i)) {
+                factorsArray[currentId] = i;
+                currentId++;
+            }
+        }
+    }
+    int current = 1;
+    for (int i = 0; i < currentId; ++i) {
+        current *= factorsArray[i];
+    }
+
+    if (current == number) {
+        for (int i = 0; i < currentId; ++i) {
+            if (i == 0) {
+                printf("%d", factorsArray[i]);
+            }else{
+                printf(" X %d", factorsArray[i]);
+
+            }
+        }
+    } else {
+        int aux = number / current;
+
+        int idaux = 0;
+        int aux2 = 1;
+        bool p = false;
+        while (true) {
+            if (aux == aux2) {
+                break;
+            }
+            aux2 *= factorsArray[idaux];
+            potArray[idaux] += 1;
+            if (aux2 > aux) {
+                aux2 = aux2 / factorsArray[idaux];
+                if (potArray[idaux] - 2 < 0 ) {
+                    current *= pow(factorsArray[idaux], 0);
+                    potArray[idaux] = 0;
+                }else{
+
+                    current *= pow(factorsArray[idaux], potArray[idaux] - 2);
+                }
+                aux = number / current;
+                aux2 = 1;
+                idaux++;
+                p = true;
+            }
+        }
+        if (p) {
+            potArray[0] -= 2;
+        }
+        for (int i = 0; i < currentId; ++i) {
+            if (i == 0) {
+                printf("%d", factorsArray[i]);
+            }else{
+                printf(" X %d", factorsArray[i]);
+
+            }
+            if (potArray[i] > 0) {
+                printf("^%d", potArray[i] + 1);
+            }
+        }
+    }
+
+    currentId = 0;
+    for (int i = 0; i < 10; ++i) {
+        factorsArray[i] = 0;
+        potArray[i] = 0;
+    }
+}
 
 int main(int argc, char **argv){
 	int n, result;
@@ -68,6 +147,8 @@ int main(int argc, char **argv){
             else {
                 printf("%d", result);
             }
+            printf(" -> ");
+            primesFactors(result);
             printf("\n");
         }
     }
