18f15a91170601d48957e01e44c3209991d97708
Fabian Rojas <fabian.rojas02@alumnos.ucn.cl>
2021-12-10 01:33:59 -0300

    feat: add prime number checker

--- fibonacci.c
+++ fibonacci.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>
+#include<stdbool.h>
+
+void reset () {
+    printf("\033[0m");
+}
+
+void red (int n) {
+    printf("\033[1;31m%d", n);
+    reset();
+}
+
+void green(int n) {
+    printf("\033[0;32m%d", n);
+    reset();
+}
 
 int fib(int n) {
 	int i, j;
@@ -16,17 +31,44 @@ int fib(int n) {
 	}
 }
 
+bool isPrime(int n) {
+
+    if (n <= 0) return false;
+    if (n == 1) return false;
+    if (n == 2) return true;
+    if (n % 2 == 0) return false;
+    for (int i = 3; i * i <= n; i += 2) {
+        if ((n % i) == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
 int main(int argc, char **argv){
 	int n, result;
 	char *a = argv[1];
     n = atoi(a);
-
 	#pragma omp parallel
     {
 		#pragma omp single
         for (int i = 0; i <= n; ++i) {
             result = fib(i);
-            printf("%d: %d\n",i,  result);
+
+            if (isPrime(i)) {
+                red(i);
+            }else {
+                printf("%d",i);
+            }
+            printf(": ");
+            if (isPrime(result)) {
+                green(result);
+            }
+            else {
+                printf("%d", result);
+            }
+            printf("\n");
         }
     }
 }
